d098253083b7b6528c31306875bb8a1d022e2f67
pyu3 <pyu3@mail.sfsu.edu>
2019-02-11 19:26:06 -0800

    Created a network request for superhero movies

--- Flix/View Controllers/MovieGridViewController.swift
+++ Flix/View Controllers/MovieGridViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class MovieGridViewController: UIViewController {
 
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=***REMOVED***&language=en-US&page=1")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                // Get the array of movies
+                // Store the movies in a property to use elsewhere
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                // Reload your table view data
+//                self.tableView.reloadData()
+                
+                print(self.movies)
+            }
+        }
+        
+        task.resume()
         
     } // end viewDidLoad function
     
